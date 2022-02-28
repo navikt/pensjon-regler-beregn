caa3be94e4b11b938a2556bcc2cf80930d496c03
W147791 <bian.wu@nav.no>
2022-02-28 16:07:46 +0100

    test with json format 1. add table  template

--- app/src/components/ResponsePane.js
+++ app/src/components/ResponsePane.js
@@ -15,7 +15,7 @@ const  openTab = function (evt, tabName) {
     for (i = 0; i<tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    // console.log("click:" + JSON.stringify(tabName))
+    //console.log("click:" + JSON.stringify(tabName))
     document.getElementById(tabName.name).style.display = "block";
     evt.currentTarget.className += " active";
     //return undefined;
@@ -45,21 +45,19 @@ function ResponsePane(props) {
             for (var k in uiData) {
               if (uiData.hasOwnProperty(k)) {
                   const name = uiData[k]['name']
-                  console.log(name)
-                     //const Element = "button" +  ' className="tablinks"' + ' onClick=\"openTab(event, \'' + name + "')\""
-                    // const horizontalTab =<button className="tablinks" onClick={this.openTab.bind(this, {name})}>{name}</button>
+                  // console.log(name)
                   const horizontalTab =<button className="tablinks" onClick={(e) => openTab(e, {name})}>{name}</button>
-                  //const horizontalTab =<Element>{name}</Element>
                   rootTab.push(horizontalTab)
 
                   const secondLevel = uiData[k]['data']['a']
                   const tabname = uiData[k]['name']
-
+                  var eachTab = []
                   for(var j in secondLevel) {
                       //console.log("tab or subList level:" + JSON.stringify(secondLevel[j]))
                         //create TabContent (subtabList or  tables)
                       const typeValue = secondLevel[j]['type']
-                      var eachTab = []
+                      const orientation = secondLevel[j]['orientation']
+
                       if (typeValue == 'TABLIST') {
                           //TODO loop all the subTab
 
@@ -68,35 +66,77 @@ function ResponsePane(props) {
                       }
                       else if(typeValue == "TABLE") {
                           var t = []
-
-                          //header
-                          var rows = secondLevel[j]['header']
-                          var cells = []
-                          for(var k in rows) {
-                              const cell = <th>{rows[k]['data']}</th>  //FIXME
-                              cells.push(cell)
+                          let tableName = ''
+                          if(secondLevel[j].hasOwnProperty('name')) {
+                              tableName =secondLevel[j]['name']
+                              eachTab.push(<h3>{tableName}</h3>)
                           }
-                          const header = <tr>{cells}</tr>
-                          t.push(header)
-
-                          //data
-                          rows = secondLevel[j]['data']['List']
-                          cells = []
-                          for(var k in rows) {
-                              //TODO cell can have popup dialog.
-                              const cell = <td>{rows['data']}</td>
-                              cells.push(cell)
+                          if(orientation=='VERTICAL') {
+                              const headers = secondLevel[j]['header'];
+                              //console.log( secondLevel[j]['data'] )
+                              const data = secondLevel[j]['data']['entry']['list'];
+                              for(var i in headers) {
+                                  var r = []
+                                  const header = <th>{headers[i]['data']}</th>
+                                  //suppose just one cell for test
+                                  // console.log( data[i]['data'])
+                                  const cell = <td>{data[i]['data']}</td>
+                                  r.push(header)
+                                  r.push(cell)
+                                  t.push(<tr>{r}</tr>)
+                              }
                           }
-                          const onerow = <tr>{cells}</tr>
-                          t.push(onerow)
+                          else if(orientation=='HORIZONTAL'){
+                              //header
+                              const headers = secondLevel[j]['header'];
+                              var header = []
+                              for(var i in headers) {
+                                  const cell = <th>{headers[i]['data']}</th>
+                                  header.push(cell)
+                              }
+                              t.push(<tr>{header}</tr>)
 
+                              //data
+                              var r = []
+                              let data = []
+                              //console.log(secondLevel[j]['data'])
+                              if(secondLevel[j]['data'].hasOwnProperty('entry')) {
+                                if(secondLevel[j]['data']['entry'].hasOwnProperty('list')) {
+                                      data = secondLevel[j]['data']['entry']['list'];
+                                    for(var i in data) {
+                                        //TODO cell can have popup dialog.
+                                        const cell = <td>{data[i]['data']}</td>
+                                        r.push(cell)
+                                    }
+                                    t.push(<tr>{r}</tr>)
+                                  }
+                              }else   {
+                                  data = secondLevel[j]['data'];
+                                  for(var i in data) {
+                                      for(var x in data[i]) {
+                                          // console.log("x: " + x)
+                                          if(x =='list') {
+                                              for(var c in data[i]['list']) {
+                                                  console.log(data[i]['list'][c]['data'])
+                                                  //TODO cell can have popup dialog.
+                                                  const cell = <td>{data[i]['list'][c]['data']}</td>
+                                                  r.push(cell)
+                                              }
+                                              t.push(<tr>{r}</tr>)
+                                          }
+                                      }
+                                  }
+                              }
+
+                          }
                           //create a table
-                          t = <table>{t}</table>
-                          eachTab.push(t)
+                          // t = <table><tbody>{t}</tbody></table>
+                          eachTab.push(<table><tbody>{t}</tbody></table>)
                       }
-                      eachTab = <div id={tabname} className="tabcontent">{eachTab}</div>  //sub level , need wrap to equal
+                      //eachTab = <div id={tabname} className="tabcontent">{eachTab}</div>  //sub level , need wrap to equal
+                      tabs.push(<div id={tabname} className="tabcontent">{eachTab}</div>)  //same level , use push to merge.
                   }
-                  tabs.push(eachTab)  //use level , use push to merge.
+                  eachTab= []
               }
            }
 
@@ -108,103 +148,103 @@ function ResponsePane(props) {
     }
 
 
-    // use this recursive function with a parse funciton
-    function parseObjectProperties(obj, parse) {
-        for (var k in obj) {
-            if (typeof obj[k] === 'object' && obj[k] !== null) {
-                parseObjectProperties(obj[k], parse)
-            } else if (obj.hasOwnProperty(k)) {
-                parse(k, obj[k], obj)
-            }
-        }
-    }
+//     // use this recursive function with a parse funciton
+//     function parseObjectProperties(obj, parse) {
+//         for (var k in obj) {
+//             if (typeof obj[k] === 'object' && obj[k] !== null) {
+//                 parseObjectProperties(obj[k], parse)
+//             } else if (obj.hasOwnProperty(k)) {
+//                 parse(k, obj[k], obj)
+//             }
+//         }
+//     }
+//
+//
+// // then apply to the property the task you want, in this case just console
+//     const replace = "replaceTextString"
+//
+//     function createHtml(k, prop, obj) {
+//         if (k == 'type') {
+//             if (prop == 'TABLIST') {
+//                 console.log(k + ': ' + prop)
+//                 const tabList = <div className="tab"></div>
+//                 uiHtml = tabList
+//             } else if (prop == 'TAB') {
+//                 console.log(k + ': ' + prop)
+//                 //<button class="tablinks" onclick="openCity(event, 'London')" id="defaultOpen">London</button>
+//                 const name = obj['name']
+//                 const horizontalTab = <button className="tablinks" onClick="openTab(event,{name})">{name}</button>
+//                 uiHtml = uiHtml.toString().replace(replace, horizontalTab + replace)
+//                 //document.getElementById("demo").innerHTML =  uiHtml
+//             } else if (prop == 'Table') {
+//
+//             }
+//         }
+//
+//     }
 
+    // parseObjectProperties(uiData, function (k, prop, obj) {
+    //     //createHtml(k,prop,obj)
+    //     createblock(k, prop, obj)
+    // })
+    //
+    // function createblock(type, prop, obj) {
+    //     switch (type) {
+    //         // case "TABLIST":
+    //         //     const Element = "h" + data.level;
+    //         //     return <Element>{data.text}</Element>;
+    //         case "TAB":
+    //             const name = obj['name']
+    //             const horizontalTab = "<button className=\"tablinks\" onClick=\"openTab(event, '" + name + "')\">" + name + "</button>"
+    //             return horizontalTab;
+    //         case "Table":
+    //
+    //
+    //         // default:
+    //         //     console.log("Unknown block type", type);
+    //         //     return null;
+    //     }
+    // }
 
-// then apply to the property the task you want, in this case just console
-    const replace = "replaceTextString"
-
-    function createHtml(k, prop, obj) {
-        if (k == 'type') {
-            if (prop == 'TABLIST') {
-                console.log(k + ': ' + prop)
-                const tabList = <div className="tab"></div>
-                uiHtml = tabList
-            } else if (prop == 'TAB') {
-                console.log(k + ': ' + prop)
-                //<button class="tablinks" onclick="openCity(event, 'London')" id="defaultOpen">London</button>
-                const name = obj['name']
-                const horizontalTab = <button className="tablinks" onClick="openTab(event,{name})">{name}</button>
-                uiHtml = uiHtml.toString().replace(replace, horizontalTab + replace)
-                //document.getElementById("demo").innerHTML =  uiHtml
-            } else if (prop == 'Table') {
-
-            }
-        }
-
-    }
-
-    parseObjectProperties(uiData, function (k, prop, obj) {
-        //createHtml(k,prop,obj)
-        createblock(k, prop, obj)
-    })
-
-    function createblock(type, prop, obj) {
-        switch (type) {
-            // case "TABLIST":
-            //     const Element = "h" + data.level;
-            //     return <Element>{data.text}</Element>;
-            case "TAB":
-                const name = obj['name']
-                const horizontalTab = "<button className=\"tablinks\" onClick=\"openTab(event, '" + name + "')\">" + name + "</button>"
-                return horizontalTab;
-            case "Table":
-
-
-            // default:
-            //     console.log("Unknown block type", type);
-            //     return null;
-        }
-    }
-
-    const blocks = {
-        time: 1602725895949,
-        blocks: [
-            {
-                type: "header",
-                data: {
-                    text: "This is a heading",
-                    level: 2
-                }
-            },
-            {
-                type: "paragraph",
-                data: {
-                    text: "This is a paragraph"
-                }
-            }
-        ]
-    };
-
-    function Block(block) {
-        const type = block.type
-        const name = block.name
-        const data = block.data
-        switch (type) {
-            // case "header":
-            //     const Element = "h" + data.level;
-            //     return <Element>{data.text}</Element>;
-            // case "paragraph":
-            //     return <p>{data.text}</p>;
-            case "TABLIST":
-                const tabList = <div class="tab"></div>
-                return tabList;
-            case "TAB":
-                const horizontalTab = <button className="tablinks" onClick="openTab(event,{name})">{name}</button>
-                return horizontalTab;
-            case "Table":
-
-                    }
-        }
+    // const blocks = {
+    //     time: 1602725895949,
+    //     blocks: [
+    //         {
+    //             type: "header",
+    //             data: {
+    //                 text: "This is a heading",
+    //                 level: 2
+    //             }
+    //         },
+    //         {
+    //             type: "paragraph",
+    //             data: {
+    //                 text: "This is a paragraph"
+    //             }
+    //         }
+    //     ]
+    // };
+    //
+    // function Block(block) {
+    //     const type = block.type
+    //     const name = block.name
+    //     const data = block.data
+    //     switch (type) {
+    //         // case "header":
+    //         //     const Element = "h" + data.level;
+    //         //     return <Element>{data.text}</Element>;
+    //         // case "paragraph":
+    //         //     return <p>{data.text}</p>;
+    //         case "TABLIST":
+    //             const tabList = <div class="tab"></div>
+    //             return tabList;
+    //         case "TAB":
+    //             const horizontalTab = <button className="tablinks" onClick="openTab(event,{name})">{name}</button>
+    //             return horizontalTab;
+    //         case "Table":
+    //
+    //                 }
+    //     }
 
     return (
         <div className="ResponsePane">
@@ -213,42 +253,7 @@ function ResponsePane(props) {
             {/*<p>response result: {result.xml}</p>*/}
             <p id="demo"></p>
             {parseJson()}
-            {/*<div className="tab">
-                <button className="tablinks" onClick="openTab(event, 'Oversikt')">Oversikt</button>
-                <button className="tablinks" onClick="openTab(event, 'GenerellHistorikk')">GenerellHistorikk</button>
-                <button className="tablinks" onClick="openTab(event, 'Pensjonsbeholdning')">Pensjonsbeholdning</button>
-                <button className="tablinks" onClick="openTab(event, 'Uføre')">Uføre</button>
-                <button className="tablinks" onClick="openTab(event, 'AFP Historikk')">AFP Historikk</button>
-                <button className="tablinks" onClick="openTab(event, 'Opptjeningsgrunnlag')">Opptjeningsgrunnlag
-                </button>
-                <button className="tablinks" onClick="openTab(event, 'Inntektsgrunnlag')">Inntektsgrunnlag</button>
-                <button className="tablinks" onClick="openTab(event, 'Trygdetid kap 19')">Trygdetid kap 19</button>
-                <button className="tablinks" onClick="openTab(event, 'Trygdetid kap 20')">Trygdetid kap 20</button>
-                <button className="tablinks" onClick="openTab(event, 'Trygdetid Alternativ')">Trygdetid Alternativ
-                </button>
-                <button className="tablinks" onClick="openTab(event, 'Trygdeavtale')">Trygdeavtale</button>
-                <button className="tablinks" onClick="openTab(event, 'Institusjon')">Institusjon</button>
-                <button className="tablinks"
-                        onClick="openTab(event, 'InngangOgEksportgrunnlag')">InngangOgEksportgrunnlag
-                </button>
-                replaceTextString
-            </div>*/}
-            {/*
-            <div className="App">
-                <h1>JSON to html below</h1>
-                {blocks.blocks.map((block, i) => (
-                    <Block key={i} {...block} />
-                ))}
-            </div>*/}
-            {/*<div>
-                <h1>JSON to html below</h1>
-                {console.log(uiData.TabList.data.a[0])}
 
-                {
-                    uiData.map((block, i) => (
-                        <Block key={i} {...block} />
-                    ))}
-            </div>*/}
         </div>
     )
 }
