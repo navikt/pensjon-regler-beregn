37ab34c1bfe235829621e6030de1059d470d1c88
Levkowetz, Scott <Scott.Levkowetz@nav.no>
2022-04-21 15:53:53 +0200

    Added code for simple side TabList

--- app/src/components/GUIElements/TabList.js
+++ app/src/components/GUIElements/TabList.js
@@ -3,118 +3,89 @@ import React, {useEffect, useState} from "react";
 import {JsonParser} from './JsonParser';
 import { Tabs } from '@navikt/ds-react';
 import {Nav} from 'react-bootstrap'
+import './CSS/TabList.css'
 
 export function TabList(props){
     
     let [tabs] = useState(props.tabs);
-    const [value, setValue] = useState(props.tabs['name'])
+    const [value, setValue] = useState(props.tabs['data'][1][0]['name'])
 
     function log(){
     console.log("Inside TabList")
     console.log(tabs)
     }
+
+    function log2() {
+      console.log("SideTab")
+      console.log(tabs)
+    }
     if(tabs['position'] == "TOP") {
         return(
-            <>
-                <Tabs value = {value} onChange={setValue}> {/* notat til etter påske, dele opp Tab headers inne i Tab.List og Tab content etter Tab.list slik at
-                det ikke ødelegger CSS*/}
-                    {log()}
-                    <Tabs.List>
-                        {tabs['data'][1].map((data,key) => {
-                            return (
-                                <Tabs.Tab
-                                    value = {data['name']}
-                                    label = {data['name']}
-                                    id = {data['name'] + '-tab'}
-                                    aria-controls = {data['name'] + '-panel'}
-                                    >
-                                </Tabs.Tab>
-                            )
-                        })}
-                    </Tabs.List>
-                </Tabs>
-                {tabs['data'][1].map((data,key) => {
+          <>
+            <Tabs value = {value} onChange={setValue}>
+              {log()}
+              <Tabs.List>
+                {tabs['data'][1].map((data,key) => { //Creating header buttons for each Tab
                   return (
-                    <div
+                    <Tabs.Tab
+                      value = {data['name']}
+                      label = {data['name']}
+                      id = {data['name'] + '-tab'} //Creating references from header button to tab content
+                      aria-controls = {data['name'] + '-panel'}
+                      >
+                    </Tabs.Tab>
+                  )
+                })}
+              </Tabs.List>
+            </Tabs>
+            {tabs['data'][1].map((data,key) => { //Creating div for each tab with reference to header button
+              return (
+                <div
+                  role = "tabpanel"
+                  hidden = {value !== data['name'] }
+                  aria-labelledby = {data['name'] + '-tab'}
+                  id = {data['name'] + '-panel'}
+                  tabIndex = {0}
+                >
+                  <JsonParser data = {data}></JsonParser> {/*Sending tab content back to parser function*/}
+                </div>
+              )
+            })}
+          </>
+        );
+    } else if(tabs['position'] == "SIDE") {
+        return(
+          <div className='sidetab-container'>
+            <div className="sidetab-menu-container">
+              {tabs['data'][1].map((data,key) => {
+                return (
+                  <div 
+                    className = "sidetab-button"
+                    onClick={() => setValue(data['name'])}
+                    id = {data['name'] + '-tab'} //Creating references from header button to tab content
+                    aria-controls = {data['name'] + '-panel'}
+                  >
+                    {data['name']}
+                  </div>
+                )
+              })}
+            </div>
+            <div className='sidetab-content-container'>
+              {tabs['data'][1].map((data,key) => {
+                return (
+                  <div 
                     role = "tabpanel"
                     hidden = {value !== data['name'] }
                     aria-labelledby = {data['name'] + '-tab'}
                     id = {data['name'] + '-panel'}
-                    tabIndex = {0}
-                    >
-                      <JsonParser data = {data}></JsonParser>
-                      </div>
-                  )
-                })}
-            </>
-        );
-    } else if(tabs['position'] == "SIDE") {
-        return(
-            <Nav defaultActiveKey="/home" className="flex-column">
-                                        {tabs['data'][1].map((data,key) => {
-                            return (
-                                <JsonParser data = {data}></JsonParser>
-                            )
-                        })}
-            </Nav>
+                    tabIndex = {0}>
+                      <JsonParser data = {data}></JsonParser> {/*Sending tab content back to parser function*/}
+                  </div>
+                )
+              })}
+              </div>
+            </div>
         );
     }
 
 }
-
-
-/*
-const [value, setValue] = useState("skap");
-return (
-  <>
-    <Tabs value={value} onChange={setValue}>
-      <Tabs.List>
-        <Tabs.Tab
-          value="skap"
-          label="Skap"
-          id="skap-tab"
-          aria-controls="skap-panel"
-        />
-        <Tabs.Tab
-          value="oppvaskmaskin"
-          label="Oppvaskmaskin"
-          id="oppvaskmaskin-tab"
-          aria-controls="oppvaskmaskin-panel"
-        />
-        <Tabs.Tab
-          value="fryser"
-          label="Fryser"
-          id="fryser-tab"
-          aria-controls="fryser-panel"
-        />
-      </Tabs.List>
-    </Tabs>
-    <div
-      role="tabpanel"
-      hidden={value !== "skap"}
-      aria-labelledby="skap-tab"
-      id="skap-panel"
-      tabIndex={0}
-    >
-      Innhold skap
-    </div>
-    <div
-      role="tabpanel"
-      hidden={value !== "oppvaskmaskin"}
-      aria-labelledby="oppvaskmaskin-tab"
-      id="oppvaskmaskin-panel"
-      tabIndex={0}
-    >
-      Innhold oppvaskmaskin
-    </div>
-    <div
-      role="tabpanel"
-      hidden={value !== "fryser"}
-      aria-labelledby="fryser-tab"
-      id="fryser-panel"
-      tabIndex={0}
-    >
-      Innhold fryser
-    </div>
-  </>
-);*/
