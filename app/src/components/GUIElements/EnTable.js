8530c5f09be94bd638a99606cfe049b81e0a6e98
W147791 <bian.wu@nav.no>
2022-04-28 09:16:35 +0200

    fix layout of popover.

--- app/src/components/GUIElements/EnTable.js
+++ app/src/components/GUIElements/EnTable.js
@@ -1,56 +1,65 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useRef, useState} from "react";
 import {JsonParser} from "./JsonParser";
-import {Popover, Table} from '@navikt/ds-react';
-import {Heading, Button} from '@navikt/ds-react';
+import {Button, Heading, Popover, Table} from '@navikt/ds-react';
 import data from "bootstrap/js/src/dom/data";
 
-export function EnTable(props){
+export function EnTable(props) {
     let [table] = useState(props.table)
 
-    function log(){
+    function log() {
         console.log("Inside table", table)
     }
 
     function horizontalHeader(item, index) {
-        if(item[index]==null)
-            return
-        else if(item[index].hasOwnProperty('header'))
+        if (item[index] == null)
+            return (null)
+        else if (item[index].hasOwnProperty('header'))
             return <Table.HeaderCell scope="col">{item[index][0]['data']}</Table.HeaderCell>
     }
 
     function showRow(item) {
         //console.log("one row data" , item, index)
-        var row = []
-        if(Array.isArray(item[1])){
+        let row = []
+        if (Array.isArray(item[1])) {
             //console.log("item[index]" , item[index])
-            item[1].map((subitem,j) => {
+            item[1].map((subitem, j) => {
                 let popOver;
-                let btnCell;
 //console.log("Item inside cell" , subitem, j)
                 if (subitem['popover']) {
                     const buttonRef = useRef(null);
                     const [open, setOpen] = useState(false);
                     if (subitem['header']) {
-                        btnCell = <Table.HeaderCell scope="col"><Button ref={buttonRef} onClick={() => setOpen(true)} size="xsmall">
-                                {subitem['data']}</Button></Table.HeaderCell>;
-                        row.push(btnCell)
-                        popOver = <Popover open={open} onClose={() => setOpen(false)} anchorEl={buttonRef.current}
-                                           arrow={true} placement="auto">
-                            <Popover.Content style={{overflow:"auto"}}><JsonParser data = { subitem['popoverContent']}></JsonParser></Popover.Content>
-                        </Popover>;
+                        popOver = <Table.HeaderCell scope="col"><Button ref={buttonRef} onClick={() => setOpen(true)}
+                                                                        size="xsmall">
+                            {subitem['data']}</Button>
+                            <Popover open={open} onClose={() => setOpen(false)} anchorEl={buttonRef.current}
+                                     arrow={true} placement="auto" offset={32}>
+                                <Popover.Content><JsonParser
+                                    data={subitem['popoverContent']}></JsonParser></Popover.Content>
+                            </Popover></Table.HeaderCell>;
                         row.push(popOver)
                     } else {
-                        btnCell = <Table.DataCell><Button style={{display:"inline-block"}} ref={buttonRef} onClick={() => setOpen(true)} size="xsmall" >
-                            {subitem['data']}</Button></Table.DataCell>;
-                        row.push(btnCell)
-                        popOver = <Popover open={open} onClose={() => setOpen(false)} anchorEl={buttonRef.current}
-                                           arrow={true} placement="auto" offset={32}>
+                        popOver = <Table.DataCell><Button ref={buttonRef}
+                                                          onClick={() => setOpen(true)} size="xsmall">
+                            {subitem['data']}</Button><Popover open={open} onClose={() => setOpen(false)}
+                                                               anchorEl={buttonRef.current}
+                                                               arrow={true} placement="auto" offset={32}>
                             <Popover.Content><JsonParser
                                 data={subitem['popoverContent']}></JsonParser></Popover.Content>
-                        </Popover>;
+                        </Popover></Table.DataCell>;
                         row.push(popOver)
+                        // let modal = <Table.DataCell><Button ref={buttonRef} onClick={() => setOpen(true)} size="xsmall">
+                        //     {subitem['data']}</Button>
+                        //     <Modal open={open} onClose={() => setOpen(false)}>
+                        //         <Modal.Content>
+                        //             <JsonParser
+                        //                 data={subitem['popoverContent']}></JsonParser>
+                        //         </Modal.Content>
+                        //     </Modal>
+                        // </Table.DataCell>
+                        // row.push(modal)
                     }
-                }  else {
+                } else {
                     if (subitem['header']) {
                         row.push(<Table.HeaderCell scope="col">{subitem['data']}</Table.HeaderCell>)
                     } else {
@@ -64,17 +73,16 @@ export function EnTable(props){
     }
 
 
-    const Checkname = ({ name }) => {
+    const Checkname = ({name}) => {
         //console.log(table)
-        if(name!=null && name.includes('ingen')) {
+        if (name != null && name.includes('ingen')) {
             return (null);
-        }
-        else
+        } else
             return <div className="w-full flex flex-col gap-4">
-                <Table size="small" >
+                <Table size="small">
                     <Table.Header>
                         <Table.Row>
-                            {table['cells'].map((item,index)=> (
+                            {table['cells'].map((item, index) => (
                                 horizontalHeader(item, index)
                             ))}
                         </Table.Row>
@@ -82,7 +90,7 @@ export function EnTable(props){
                     <Table.Body>
                         {table['cells'][1].map((item, index) => (
                             <Table.Row>
-                                {showRow(item,index)}
+                                {showRow(item, index)}
                             </Table.Row>
                             // <CheckExpandableRow rowContent ={item} >
                             // </CheckExpandableRow>
@@ -93,11 +101,11 @@ export function EnTable(props){
             </div>
     }
 
-    const CheckExpandableRow =  ({ rowContent }) => {
-        var expandable = false
-        var expandableitem = []
-        if(Array.isArray(rowContent[1]) ) {
-            rowContent[1].map((subitem,j) => { //check every cell of this row, show expandable if one cell have popOver.
+    const CheckExpandableRow = ({rowContent}) => {
+        let expandable = false
+        let expandableitem = []
+        if (Array.isArray(rowContent[1])) {
+            rowContent[1].map((subitem, j) => { //check every cell of this row, show expandable if one cell have popOver.
                 if (subitem['popover']) {
                     expandable = true
                     expandableitem = subitem
@@ -105,12 +113,11 @@ export function EnTable(props){
             })
         }
 
-        if(expandable) {
-            return  <Table.ExpandableRow content={ <JsonParser data = { expandableitem['popoverContent']}></JsonParser>}>
+        if (expandable) {
+            return <Table.ExpandableRow content={<JsonParser data={expandableitem['popoverContent']}></JsonParser>}>
                 {showRow(rowContent)}
             </Table.ExpandableRow>
-        }
-        else {
+        } else {
             return <Table.Row><Table.DataCell></Table.DataCell> {showRow(rowContent)} </Table.Row>
         }
     }
@@ -121,8 +128,9 @@ export function EnTable(props){
 
             <Heading size="xsmall" level="6"> &ensp;
             </Heading>
-            <Heading spacing size="xsmall" level="6"> &ensp; {table.hasOwnProperty('name')?table['name']:''}</Heading>
-            <Checkname name = {table['name']}>
+            <Heading spacing size="xsmall"
+                     level="6"> &ensp; {table.hasOwnProperty('name') ? table['name'] : ''}</Heading>
+            <Checkname name={table['name']}>
 
             </Checkname>
         </div>
