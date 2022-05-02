6a5e2ea18053e60b7fd56f1ca190d803fed2486c
Levkowetz, Scott <Scott.Levkowetz@nav.no>
2022-05-02 09:35:12 +0200

    Implement API call to LogViewer, imlement call to pensjon-regler on run button

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,7 @@
-import { Route, Routes, HashRouter } from "react-router-dom";
+import { Route, Routes, HashRouter,useParams } from "react-router-dom";
 import './App.css';
 import React, {useEffect, useState, useRef, useCallback} from "react";
 import ReactDOM from "react-dom";
-import { useParams } from "react-router";
 
 import Header from "./components/Header";
 import HeaderButton from "./components/Small/HeaderButton";
@@ -10,52 +9,84 @@ import RequestPane from "./components/RequestPane";
 import ResponsePane from "./components/ResponsePane";
 import Footer from "./components/Footer";
 
-
-   
-
-var servicetype= 'BeregnAlderspensjon2011ForsteUttakRequest'; //http://localhost:3000/#/246355100/
-//var servicetype = 'BeregnUforetrygdRequest'
-var url = 'http://localhost:8080/api/beregn?requestType='+servicetype;
-const d = require('./components/Testdata/RequestData.json');
+//var servicetype= 'BeregnAlderspensjon2011ForsteUttakRequest'; //http://localhost:3000/#/246355100/
+//var servicetype = 'RevurderingAlderspensjon2016Request'
+//var url = 'http://localhost:8080/api/beregn?requestType='+servicetype;
+//const d = require('./components/Testdata/RequestData.json');
 //const d = require('./components/Testdata/RequestWithTree.json');
-
-
-
-
-
-
 
 export default function App() {
   //API fetch constants
+  const [metaData, setMetadata] = useState([]);
+  const [serviceType, setServiceType] = useState("")
+  const [environment, setEnvironment] = useState("");
+
   const [result, setResult] = useState([]);
   const [isSending, setIsSending] = useState(false)
+  const [isFetched, setIsFetched] = useState(false)
   const isMounted = useRef(true)
 
+  function FetchByLogID() {
+    const {id} = useParams();
+    let logUrl = 'https://pensjon-regler-logviewer-api.dev-fss.nais.io/api/log/'+id;
+    console.log("fetched outer: "+isFetched)
+    if (!isFetched){
+    console.log("fetched inner: "+isFetched)
+    const fetchLog = useCallback(async() => {
+    console.log('fetching')
+      try {
+        fetch(logUrl, {
+          method: 'GET',
+          headers: {
+            'Content-Type':  'application/json',
+            'accept': 'application/json' 
+          }
+        })
+        .then(response => response.json())
+        .then(response => setMetadata(response))
+        .then(() => setServiceType(metaData['method']))
+        .then(() => setEnvironment(metaData['environment']))
+        .then(() => setIsFetched(true))
+      } catch(error) {
+        console.log('Error:',error)
+      }
+    })
+    fetchLog();
+  }
+  return (
+    <div>
+    </div>
+    )
+  }
+
   function Request() {
-    let {id} = useParams();
     return ( //Send request ID from url to component
-        <RequestPane id = {id} request = {result.request}></RequestPane>
+        <RequestPane request = {result.request}></RequestPane>
     );
   } 
 
   function Response() {
-    console.log("Response called")
-    console.log((result))
     return (
       <ResponsePane response = {result.response}></ResponsePane>
     );
   }
 
-  // set isMounted to false when we unmount the component, usnure if neccessary
-  useEffect(() => {
-    return () => {
-      isMounted.current = false
-    }
-  }, [])
+// set isMounted to false when we unmount the component, unsure if neccessary
+useEffect(() => {
+  return () => {
+    isMounted.current = false
+  }
+}, [])
 
-const fetchData = useCallback(async() => {
+const fetchGuiModel = useCallback(async() => {
   if (isSending) return
   setIsSending(true)
+  let tjeneste = serviceType.replace("/api/","")
+  tjeneste = tjeneste.charAt(0).toUpperCase() + tjeneste.slice(1)
+  let url = 'http://localhost:8080/api/beregn?requestType='+tjeneste+'Request'
+  //let url = 'https://'+environment+'.dev.adeo.no/api/beregn?requestType='+tjeneste
+  let body = metaData['xml']
+  console.log(body)
   try {
     fetch(url, {
       method: 'POST',
@@ -64,7 +95,7 @@ const fetchData = useCallback(async() => {
           'accept': 'application/json',
           'X-pensjonregler-log': 'disabled'
       },
-      body: JSON.stringify(d)
+      body: (body)
     })
     .then(response => response.json())
     .then(response => setResult(response));
@@ -73,7 +104,7 @@ const fetchData = useCallback(async() => {
     }
     if (isMounted.current) // only update if we are still mounted
     setIsSending(false)
-},[isSending])
+},[isSending, environment, serviceType])
   
   return (
     <div className = "App">
@@ -83,15 +114,16 @@ const fetchData = useCallback(async() => {
           <div className="HeaderTitle">Beregn Pensjon</div>
           <div className="HeaderButton"> Åpne </div>
           <div className="HeaderButton"> Sats </div>
-          <div className="HeaderButton" disabled = {isSending} onClick = {fetchData}> Run </div>
+          <div className="HeaderButton" disabled = {isSending} onClick = {fetchGuiModel}> Run </div>
         </div>
       </div>
       <div className = "main-container">
         <HashRouter>
           <Routes>
-            <Route path="/:id" element={<Request />}></Route> {/* routing to enable us to read parameter from URL */}
+            <Route path="/:id" element = {<FetchByLogID/>}></Route> {/* routing to enable us to read parameter from URL */}
           </Routes>
         </HashRouter>
+        {<Request />}
         {<Response />}
       </div>
       <div className = "footer"><Footer></Footer></div>
