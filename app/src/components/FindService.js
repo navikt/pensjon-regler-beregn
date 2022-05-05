00e2b0c6a5ae0e4e20ca6e1d58e0d350a203a736
Levkowetz, Scott <Scott.Levkowetz@nav.no>
2022-05-05 09:55:05 +0200

    Completed FindService function

--- app/src/components/FindService.js
+++ app/src/components/FindService.js
@@ -1,4 +1,3 @@
-import { useState } from "react"
 
 export default function FindService(fetchedMethod) {
 
@@ -12,6 +11,29 @@ export default function FindService(fetchedMethod) {
         fetchedMethod = 'faktoromregnBeregningBatch'
     } else if (fetchedMethod === 'fastsettTrygdetid') {
         fetchedMethod = 'trygdetid'
+    } else if (fetchedMethod === 'faktoromregnInntekter') {
+        fetchedMethod == 'faktoromregnInntekterBatch'
+    } else if (fetchedMethod === 'inntektsavkortUforetrygd') {
+        fetchedMethod == 'inntektsavkortingUforetrygd'
+    } else if (fetchedMethod === 'regulerAfpPrivat') {
+        fetchedMethod == 'regulerAfpPrivatBeregning'
+    } else if (fetchedMethod === 'vilkarsprovAlderspensjonOver67') {
+        fetchedMethod == 'vilkarsprov'
+    } else if (
+        fetchedMethod === 'beregnUforepensjon' || 
+        fetchedMethod === 'beregnAlderspensjon' ||
+        fetchedMethod === 'beregnAFP' ||
+        fetchedMethod === 'beregnBarnepensjon' ||
+        fetchedMethod === 'beregnFamiliepleierpensjon' ||
+        fetchedMethod === 'beregnGjenlevendepensjon' ) {
+        fetchedMethod == 'beregnYtelse'
+    } else if (
+        fetchedMethod === 'simulerUforepensjon' ||
+        fetchedMethod === 'simulerAlderspensjon' ||
+        fetchedMethod === 'simulerBarnepensjon' ||
+        fetchedMethod === 'simulerAFP' ||
+        fetchedMethod === 'simulerVilkarsprovAFP' ) {
+            fetchedMethod == 'simulering'
     }
 
     serviceName = fetchedMethod.charAt(0).toUpperCase() + fetchedMethod.slice(1)+'Request'
