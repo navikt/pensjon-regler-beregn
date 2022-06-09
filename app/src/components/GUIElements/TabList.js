a45d1dc2996d7fa8d9efe5a35effa49db71a6056
Levkowetz, Scott <Scott.Levkowetz@nav.no>
2022-06-09 08:39:40 +0200

    Improve side tab selection

--- app/src/components/GUIElements/TabList.js
+++ app/src/components/GUIElements/TabList.js
@@ -80,14 +80,11 @@ export function TabList(props){
                     style={{
                         backgroundColor: value === data['name']+key ? "white": "#a8a1a6",
                         borderBottom: value === data['name']+key ? "": "1px solid grey",
+                        color: value === data['name']+key ? "black": "grey"
                     }}
                     key = {key}
                     id = {data['name'] + '-tab'} //Creating references from header button to tab content
                     aria-controls = {data['name'] + '-panel'}
-                    style={{
-                      backgroundColor: value === data['name']+key ? "white": "#a8a1a6",
-                      color: value === data['name']+key ? "black": "grey"
-                    }}
                   >
                     {data['name']}
                   </div>
