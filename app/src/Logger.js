f4572f379f6a3bb378d5b1efae26ed7b20ccbf4f
Levkowetz, Scott <Scott.Levkowetz@nav.no>
2022-06-28 09:58:43 +0200

    WIP Frontend logging

--- app/src/Logger.js
+++ app/src/Logger.js
@@ -1,6 +1,69 @@
-import {
-	createFrontendLogger,
-	DEFAULT_FRONTENDLOGGER_API_URL
-} from '@navikt/frontendlogger';
+(function () {
+	const context = window.frontendlogger.context ? window.frontendlogger.context : "";
+	const apiUrl = `${context}/api/logger/`;
+	const { appname } = window.frontendlogger;
 
-export const logger = createFrontendLogger('pensjon-regler-beregn', DEFAULT_FRONTENDLOGGER_API_URL);
+	
+	console.log(context)
+	console.log(apiUrl)
+	console.log(appname)
+
+  
+	function post(path, data) {
+	  data.url = window.location.href;
+	  data.userAgent = window.navigator.userAgent;
+	  data.appname = appname;
+  
+	  const xhr = new XMLHttpRequest();
+	  xhr.open("POST", apiUrl + path, true);
+	  xhr.setRequestHeader("Content-Type", "application/json");
+	  xhr.send(JSON.stringify(data));
+	}
+  
+	function log(level, data) {
+	  if (typeof data === "string") {
+		data = { message: data };
+	  }
+	  post(level, data);
+	}
+  
+	function reportEvent(name, fields, tags) {
+	  const data = {
+		name,
+		fields,
+		tags,
+	  };
+  
+	  post("event", data);
+	}
+  
+	const oldOnError = window.onerror;
+	window.onerror = function (message, url, line, column, error) {
+	  const json = {
+		message,
+		jsFileUrl: url,
+		lineNumber: line,
+		column,
+		messageIndexed: message,
+	  };
+	  if (error) {
+		json.stacktrace = error.stack ? error.stack : error;
+	  }
+	  post("error", json);
+	  if (oldOnError) {
+		oldOnError.apply(this, arguments);
+	  }
+	};
+	window.frontendlogger.info = function (data) {
+	  log("info", data);
+	};
+	window.frontendlogger.warn = function (data) {
+	  log("warn", data);
+	};
+	window.frontendlogger.error = function (data) {
+	  log("error", data);
+	};
+	window.frontendlogger.event = function (name, fields, tags) {
+	  reportEvent(name, fields, tags);
+	};
+  })();
