cc530e208863e56bf3a593b943601edc9d7bb88d
Levkowetz, Scott <Scott.Levkowetz@nav.no>
2022-06-28 10:38:25 +0200

    WIP Frontend logging - new Logger.js and html script

--- app/public/frontendlogger/Logger.js
+++ app/public/frontendlogger/Logger.js
@@ -1,69 +1,23 @@
-(function () {
-	const context = window.frontendlogger.context ? window.frontendlogger.context : "";
-	const apiUrl = `${context}/api/logger/`;
-	const { appname } = window.frontendlogger;
-
-	
-	console.log(context)
-	console.log(apiUrl)
-	console.log(appname)
-
+const logInfo = info => {
+	window.frontendlogger.info({
+	  info,
+	  stack: info.stack,
+	});
+  };
   
-	function post(path, data) {
-	  data.url = window.location.href;
-	  data.userAgent = window.navigator.userAgent;
-	  data.appname = appname;
   
-	  const xhr = new XMLHttpRequest();
-	  xhr.open("POST", apiUrl + path, true);
-	  xhr.setRequestHeader("Content-Type", "application/json");
-	  xhr.send(JSON.stringify(data));
-	}
+  const logWarn = warn => {
+	window.frontendlogger.warn({
+	  warn,
+	  stack: warn.stack,
+	});
+  };
   
-	function log(level, data) {
-	  if (typeof data === "string") {
-		data = { message: data };
-	  }
-	  post(level, data);
-	}
+  const logError = error => {
+	window.frontendlogger.error({
+	  error,
+	  stack: error.stack,
+	});
+  };
   
-	function reportEvent(name, fields, tags) {
-	  const data = {
-		name,
-		fields,
-		tags,
-	  };
-  
-	  post("event", data);
-	}
-  
-	const oldOnError = window.onerror;
-	window.onerror = function (message, url, line, column, error) {
-	  const json = {
-		message,
-		jsFileUrl: url,
-		lineNumber: line,
-		column,
-		messageIndexed: message,
-	  };
-	  if (error) {
-		json.stacktrace = error.stack ? error.stack : error;
-	  }
-	  post("error", json);
-	  if (oldOnError) {
-		oldOnError.apply(this, arguments);
-	  }
-	};
-	window.frontendlogger.info = function (data) {
-	  log("info", data);
-	};
-	window.frontendlogger.warn = function (data) {
-	  log("warn", data);
-	};
-	window.frontendlogger.error = function (data) {
-	  log("error", data);
-	};
-	window.frontendlogger.event = function (name, fields, tags) {
-	  reportEvent(name, fields, tags);
-	};
-  })();
+  export { logError as error, logInfo as info, logWarn as warn };
