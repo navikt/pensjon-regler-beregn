43f581be865a0d16d6a0d759388f0cb881e80cd9
Hellerslien, Erik <Erik.Hellerslien@nav.no>
2022-11-01 11:03:42 +0100

    Sanerer Q4 da det blir skrudd av for PEN 1.12.2022

--- app/src/components/Navigation/EnvironmentsDropdown.js
+++ app/src/components/Navigation/EnvironmentsDropdown.js
@@ -21,8 +21,8 @@ export default function EnvironmentsDropdown(props) {
             <option value={chooseEnvironemnt} /*selected disabled hidden*/>{chooseEnvironemnt}</option>
             <option value={Local_Environemnt}>{Local_Environemnt}</option>
             <option value="pensjon-regler-q0">pensjon-regler-q0</option>
+            <option value="pensjon-regler-q0">pensjon-regler-q1</option>
             <option value="pensjon-regler-q2">pensjon-regler-q2</option>
-            <option value="pensjon-regler-q4">pensjon-regler-q4</option>
             <option value="pensjon-regler-q5">pensjon-regler-q5</option>
         </Select>
     )
