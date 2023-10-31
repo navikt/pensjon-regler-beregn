cdfe5c12b33a53b5f74e49d534544cb7cc7a65b6
tordbjorn.wang.eriksen <tordbjorn.wang.eriksen@nav.no>
2023-10-31 11:49:04 +0100

    Fjernet alt gammelt

--- app/src/components/Navigation/Bruksanvisning.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Bruksanvisning() {
-    return (
-        <div>
-            <a href="/doc/index.html" className="bruksanvisning">Bruksanvisning</a>
-        </div>
-    )
-}
