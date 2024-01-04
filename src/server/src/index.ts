7dc4dea330a02c497b46ff4e76c7b5ea4d6d0c7d
tordbjorn.wang.eriksen <tordbjorn.wang.eriksen@nav.no>
2024-01-04 08:27:32 +0100

    - Hardkodet port for server (enn så lenge) - Endringer i Dockerfile (for å få innstallert dependencies)

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -1,7 +1,7 @@
 import express, {Application, Response} from 'express'
 
 const app: Application = express()
-const port = process.env.PORT || 8080
+const port = 8080
 
 app.get('/isAlive', (res: Response) => {
     res.status(200).json("OK")
