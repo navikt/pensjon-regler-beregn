720e53280f4d76d276c4a9f9336e58dc2c1dc0ed
tordbjorn.wang.eriksen <tordbjorn.wang.eriksen@nav.no>
2024-07-17 14:49:13 +0200

    - Refaktoert Table.jsx som skal løse problemet med rendering av vertikale rader i tabellene. - Ryddet opp i kode - Tilpasset responsiv visning

--- src/components/guielements/Table.jsx
+++ src/components/guielements/Table.jsx
@@ -7,43 +7,56 @@ export function EnTable(props) {
     let [table] = useState(props.table)
 
     function horizontalHeader(item, index) {
-        if (item[index] == null || undefined)
-            return (null)
-        else if (Object.hasOwn(item[index],'header') ) {
-            return <Table.HeaderCell key={index} scope="col">{item[index]['data']}</Table.HeaderCell>
-            }
+        if (item[index] == null || item[index] == undefined) {
+            return null;
+        } else if (Object.hasOwn(item[index], 'header') && item[index]['header'] === true) {
+            return <Table.HeaderCell key={index} scope="col">{item[index]['data']}</Table.HeaderCell>;
+        }
     }
 
-    function showRow(item) {
-        let row = []
+    function verticalHeader(item, index) {
+        if (item == null || item == undefined) {
+            return null;
+        } else if (Object.hasOwn(item, 'header') && item['header'] === true) {
+            return <Table.HeaderCell key={index} scope="row">{item['data']}</Table.HeaderCell>;
+        }
+    }
+
+    function showRow(item, isVertical = false, index) {
+        let row = [];
         if (Array.isArray(item)) {
             item.map((subitem, j) => {
-                row.push(<Cell element={subitem} j={j}></Cell>)
-            })
+                row.push(<Cell key={`cell-${index}-${j}`} index={index} element={subitem} j={j} isVertical={isVertical} />);
+            });
         }
-        return row
+        return row;
     }
 
     const Checkname = ({ name }) => {
         if (name != null && name.includes('Ingen')) {
-            return (null);
+            return null;
         } else
-            return <div className="w-full flex flex-col gap-4"  >
+            return <div className="w-full flex flex-col gap-4">
                 <Table size="small">
                     <Table.Header>
-                        <Table.Row>
-                            {table['cells'].map((item, index) => (
-                                horizontalHeader(item, index)
+                        <Table.Row >
+                            {table['orientation'] === 'HORIZONTAL' && table['cells'][0].map((item, index) => (
+                                horizontalHeader(table['cells'][0], index)
                             ))}
                         </Table.Row>
                     </Table.Header>
                     <Table.Body>
-                        {table['cells'].map((item, index) => (
-                            <Table.Row key={index}>
+                        {table['orientation'] === 'HORIZONTAL' && table['cells'].slice(1).map((item, index) => (
+                            <Table.Row key={`row-${index}`}>
                                 {showRow(item, index)}
                             </Table.Row>
-                        ))
-                        }
+                        ))}
+                        {table['orientation'] === 'VERTICAL' && table['cells'].map((item, index) => (
+                            <Table.Row key={index}>
+                                {verticalHeader(item[0])}
+                                {showRow(item.slice(1), true)}
+                            </Table.Row>
+                        ))}
                     </Table.Body>
                 </Table>
             </div>
@@ -54,15 +67,15 @@ export function EnTable(props) {
             <Heading size="xsmall" level="6"> &ensp;
             </Heading>
             <Heading spacing className={"tableHeading"}
-                style={{
-                    borderBottom: Object.hasOwn(table,'name') ? "2px solid grey" : "",
-                    borderTop: Object.hasOwn(table,'name') ? "2px solid grey" : "",
-                    backgroundColor: Object.hasOwn(table,'name') ? "#f1f1f1" : "",
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center'
-                }}
-                level="6"> &ensp; {Object.hasOwn(table,'name') ? table['name'] : ''}</Heading>
+                     style={{
+                         borderBottom: Object.hasOwn(table, 'name') ? "2px solid grey" : "",
+                         borderTop: Object.hasOwn(table, 'name') ? "2px solid grey" : "",
+                         backgroundColor: Object.hasOwn(table, 'name') ? "#f1f1f1" : "",
+                         display: 'flex',
+                         flexDirection: 'column',
+                         alignItems: 'center'
+                     }}
+                     level="6"> &ensp; {Object.hasOwn(table, 'name') ? table['name'] : ''}</Heading>
             <Checkname name={table['name']} className={"tableBody"}>
 
             </Checkname>
