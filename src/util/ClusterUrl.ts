1626cac783c9ae234620f0be23b9693f4eb63f7c
tordbjorn.wang.eriksen <tordbjorn.wang.eriksen@nav.no>
2025-10-01 15:35:02 +0200

    Tilpasninger for utfasing av FSS

--- src/util/ClusterUrl.ts
+++ src/util/ClusterUrl.ts
@@ -1,15 +1,5 @@
-// TODO Denne vil forsvinner når vi fjerner FSS, kan bruke URIs.ts da.
-
 export function getResponseFormat(): 'xml' | 'json' {
     const cluster = import.meta.env.VITE_CLUSTER;
     return cluster === 'FSS' ? 'xml' : 'json';
 }
 
-export function getBaseUrl(): string {
-    const cluster = import.meta.env.VITE_CLUSTER as 'GCP' | 'FSS';
-    const baseUrls: Record<'GCP' | 'FSS', string> = {
-        GCP: 'https://pensjon-regler-logger-dev.intern.dev.nav.no',
-        FSS: 'https://pensjon-regler-logviewer-api.dev.adeo.no',
-    };
-    return baseUrls[cluster] || baseUrls.FSS;
-}
---
+++ src/util/URIs.ts
@@ -0,0 +1,7 @@
+export const PENSJON_REGLER_LOGGER_URL = 'https://pensjon-regler-logger-dev.intern.dev.nav.no';
+
+export const PENSJON_REGLER_Q2_URl = 'https://pensjon-regler-q2.intern.dev.nav.no';
+
+export const PENSJON_REGLER_LOCAL_URl = 'http://localhost:8080';
+
+export const ALLE_SATSTABELLER_URL = `PENSJON_REGLER_Q2_URl${"/alleSatstabeller"}`;
