d8a10c85f3edba86d369bb0b97b8e3b7832eeb4d
ehellerslien <erik.hellerslien@nav.no>
2025-11-06 13:13:48 +0100

    Revert "Feil url for satstabeller"
    This reverts commit cb756ca89eac58f29b0cae7fc04aac01fb7082c7.

--- src/util/URIs.ts
+++ src/util/URIs.ts
@@ -4,4 +4,4 @@ export const PENSJON_REGLER_URL_PREFIX = 'http://pensjon-regler';
 
 export const PENSJON_REGLER_LOCAL_URl = 'http://localhost:8080';
 
-export const ALLE_SATSTABELLER_URL = `${PENSJON_REGLER_URL_PREFIX}-q2/alleSatstabeller`;
+export const ALLE_SATSTABELLER_URL = `${PENSJON_REGLER_URL_PREFIX}/alleSatstabeller`;
