b3a873ca3746c05e70b3a3fbc968d3dc16e86643
ehellerslien <erik.hellerslien@nav.no>
2025-11-06 13:05:05 +0100

    Bruker intern service url for kall til regler og logger

--- src/util/URIs.ts
+++ src/util/URIs.ts
@@ -1,7 +1,7 @@
-export const PENSJON_REGLER_LOGGER_URL = 'https://pensjon-regler-logger-dev.intern.dev.nav.no';
+export const PENSJON_REGLER_LOGGER_URL = 'http://pensjon-regler-logger-dev';
 
-export const PENSJON_REGLER_Q2_URl = 'https://pensjon-regler-q2.intern.dev.nav.no';
+export const PENSJON_REGLER_URL_PREFIX = 'http://pensjon-regler';
 
 export const PENSJON_REGLER_LOCAL_URl = 'http://localhost:8080';
 
-export const ALLE_SATSTABELLER_URL = `${PENSJON_REGLER_Q2_URl}/alleSatstabeller`;
+export const ALLE_SATSTABELLER_URL = `${PENSJON_REGLER_URL_PREFIX}/alleSatstabeller`;
