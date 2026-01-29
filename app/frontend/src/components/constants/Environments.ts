294aa2439dfa9877c4ef9381a368a76d7048d951
ehellerslien <erik.hellerslien@nav.no>
2026-01-29 23:10:00 +0100

    Logging for MODE i Environments.ts

--- app/frontend/src/components/constants/Environments.ts
+++ app/frontend/src/components/constants/Environments.ts
@@ -1,4 +1,5 @@
 console.log("VITE_PENSJON_ACCESS value:", import.meta.env.VITE_PENSJON_ACCESS);
+console.log("VITE MODE:", import.meta.env.MODE);
 console.log("VITE_PENSJON_ACCESS === 'prod':", import.meta.env.VITE_PENSJON_ACCESS === "prod");
 
 const environments: string[] = import.meta.env.VITE_PENSJON_ACCESS === "prod" ?
