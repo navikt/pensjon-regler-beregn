967661163324610df4d02e9020abc84f908e0e2f
tordbjorn.wang.eriksen <tordbjorn.wang.eriksen@nav.no>
2026-01-29 14:42:39 +0100

    Korrigert vite react plugin

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,6 +1,6 @@
 // typescript
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 export default ({ mode }: { mode: string }) => {
   const env = loadEnv(mode, process.cwd(), '');
