f1a6ce98987908a6fa891681b5bc779fe3a86335
ehellerslien <erik.hellerslien@nav.no>
2026-02-11 10:44:53 +0100

    Skrur på autentisering for alle miljøer. Det blir noe rest med dev-gcp / prod-gcp, da jeg vil kanskje bruke noe tilsvarende når jeg fristiller Q2 fra EntraID.

--- app/bff/src/route/routes.ts
+++ app/bff/src/route/routes.ts
@@ -19,23 +19,17 @@ export default (): Router => {
         expressRouter.use([API_LOG_BY_ID, API_BEREGN, API_CONVERT_RESPONSE, API_ALLE_SATSTABELLER], ensureAuthenticated());
     }
 
+    // Kall til pensjon-regler, skal ikke ha autentisering
     expressRouter.post(API_CONVERT_RESPONSE, async (req, res) => {
         try {
             const { env } = req.params;
-            const { className } = req.query;
-            const { sats } = req.query;
-            const { body } = req;
             const { requestUrl } = setPensjonReglerRequestScopeAndUrlForEnvironment(env);
 
-            logInfoLevel("Fikk request på /api/convertResponse med query:", req);
             const CONVERT_RESPONSE_URL = `${requestUrl}/api/convertResponse`;
             logInfo(`Kaller pensjon-beregn API på ${CONVERT_RESPONSE_URL}`);
-            logInfo(`Med className: ${className}, sats: ${sats}`);
-            logInfo(`body: ${JSON.stringify(body)}`);
 
             const response = await postApiRequest(req, {url: CONVERT_RESPONSE_URL, authenticationEnabled: false, query: req.query}, req.body);
             logInfo(`Kall til ${CONVERT_RESPONSE_URL} fullført`);
-            logInfo(`Response data: ${JSON.stringify(response.data)}`);
             const guiModel: GuiModel = response.data;
             return res.status(200).json(guiModel);
         } catch (err: unknown) {
@@ -48,21 +42,15 @@ export default (): Router => {
         }
     });
 
+    // Kall til pensjon-regler, skal ikke ha autentisering
     expressRouter.post(API_BEREGN, async (req, res) => {
         try {
             const { env } = req.params;
-            const { className } = req.query;
-            const { sats } = req.query;
-            const { body } = req;
             const { requestUrl } = setPensjonReglerRequestScopeAndUrlForEnvironment(env);
-            logInfoLevel("Fikk request på /api/beregn med query:", req);
             const BEREGN_URL = `${requestUrl}/api/beregn`;
             logInfo(`Kaller pensjon-beregn API på ${BEREGN_URL}`);
-            logInfo(`Med className: ${className}, sats: ${sats}`);
-            logInfo(`body: ${JSON.stringify(body)}`);
             const response = await postApiRequest(req, {url: BEREGN_URL, authenticationEnabled: false, query: req.query}, req.body);
             logInfo(`Kall til ${BEREGN_URL} fullført`);
-            logInfo(`Response data: ${JSON.stringify(response.data)}`);
             const guiModel: GuiModel = response.data;
             return res.status(200).json(guiModel);
         } catch (err: unknown) {
@@ -75,13 +63,13 @@ export default (): Router => {
         }
     });
 
+    // Kall til pensjon-regler, skal ikke ha autentisering
     expressRouter.get(API_ALLE_SATSTABELLER, async (req, res ) => {
         try {
             const { env } = req.params;
             logInfo(`satstabeller fra miljø: ${env}`);
             const { requestUrl } = setPensjonReglerRequestScopeAndUrlForEnvironment(env);
             logInfo(`Henter satstabeller fra miljø: ${env} med url: ${requestUrl}`);
-            logInfoLevel("Fikk request på /api/:env/alleSatstabeller med query: ", req);
 
             const SATSTABELL_URL = `${requestUrl}/alleSatstabeller`;
             logInfo(`Kaller pensjon-beregn API på ${SATSTABELL_URL}`);
@@ -98,18 +86,21 @@ export default (): Router => {
         }
     });
 
+    // Kall til pensjon-regler-logger, må autentiseres
     expressRouter.get(API_LOG_BY_ID, async (req, res) => {
         try {
-            logInfoLevel("Fikk request på /api/log/:id med query:", req);
 
             const {id} = req.params;
 
             const LOGGER_API_URL = `${serverConfiguration.pensjonReglerLoggerAppURI}/api/log/${id}`;
             logInfo(`Kaller logger API på ${LOGGER_API_URL}`);
 
-            const response = serverConfiguration.enableAccessControl
-                ? await fetchApiRequest(req, {url: LOGGER_API_URL, scope: serverConfiguration.pensjonReglerLoggerScope, authenticationEnabled: true})
-                : await fetchApiRequest(req, {url: LOGGER_API_URL, scope: serverConfiguration.pensjonReglerLoggerScope, authenticationEnabled: false});
+            // TODO Fjerner ikke enda, da vi trenger trolig noe tilsvarende når vi fristiller Q2 fra EntraID
+            // const response = serverConfiguration.enableAccessControl
+            //     ? await fetchApiRequest(req, {url: LOGGER_API_URL, scope: serverConfiguration.pensjonReglerLoggerScope, authenticationEnabled: true})
+            //     : await fetchApiRequest(req, {url: LOGGER_API_URL, scope: serverConfiguration.pensjonReglerLoggerScope, authenticationEnabled: false});
+
+            const response = await fetchApiRequest(req, {url: LOGGER_API_URL, scope: serverConfiguration.pensjonReglerLoggerScope, authenticationEnabled: true})
 
             const logResponse: LogResponse = response.data;
             logInfo(`Logger API response: ${JSON.stringify(logResponse)}`);
