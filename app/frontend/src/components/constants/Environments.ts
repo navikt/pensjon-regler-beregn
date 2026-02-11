df4a5897f65c9eb0b44de8667355f674dcbdfcc3
ehellerslien <erik.hellerslien@nav.no>
2026-02-11 11:47:20 +0100

    Legger til q0 for dev valg, ikke prod

--- app/frontend/src/components/constants/Environments.ts
+++ app/frontend/src/components/constants/Environments.ts
@@ -4,9 +4,9 @@ console.log("VITE_PENSJON_ACCESS === 'prod':", import.meta.env.VITE_PENSJON_ACCE
 
 const environments: string[] = import.meta.env.VITE_PENSJON_ACCESS === "prod" ?
     [
-        "pensjon-regler",
-        "pensjon-regler-q0"
+        "pensjon-regler"
     ] : [
+        "pensjon-regler-q0",
         "pensjon-regler-q1",
         "pensjon-regler-q2",
         "pensjon-regler-q5"
